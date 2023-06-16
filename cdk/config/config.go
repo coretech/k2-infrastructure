5d5622ed9d9593a72358cfbf21e4c36c8d94f3ea
Ilya Kholodov <ilya.kholodov@idt.net>
2023-06-16 10:36:45 +0600

    CloudFront distribution initial implementation

--- cdk/config/config.go
+++ cdk/config/config.go
@@ -1,8 +1,9 @@
 package config
 
 import (
-	"github.com/aws/jsii-runtime-go"
 	"strings"
+
+	"github.com/aws/jsii-runtime-go"
 )
 
 type Environment string
@@ -15,15 +16,17 @@ const (
 const (
 	devAccount       = "041584911022"
 	devRegion        = "us-east-1"
-	devAllocatedCIDR = "10.130.136.0/23"
+	devAllocatedCIDR = "10.130.144.0/21"
 )
 
 const (
 	prodAccount       = "133360657404"
 	prodRegion        = "us-east-1"
-	prodAllocatedCIDR = "10.200.46.0/23"
+	prodAllocatedCIDR = "10.200.64.0/21"
 )
 
+// Parse parses the environment to deploy infrastructure to from K2_ENV_NAME environment variable.
+// If no such variable is set, Parse will return k2-dev.
 func Parse(envName string) Environment {
 	if strings.EqualFold(envName, string(prodEnvironment)) {
 		return prodEnvironment
